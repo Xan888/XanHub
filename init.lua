83f72be07f59af30d78a15aa4e2f70569fcdf8a3
Xan <young@worker.com>
2026-03-12 10:44:53 +0000

    a

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 85896571713843 then
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xan888/XanHub/refs/heads/main/bgsi.lua"))()
 end
