42ec886c8ace767d21466aba07ea6b1dd4337bee
Xan <young@worker.com>
2026-03-12 11:47:14 +0000

    new loader

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
-if game.PlaceId == 85896571713843 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xan888/XanHub/refs/heads/main/bgsi.lua"))()
+local games = {
+    ["bgsi"] = {
+        Name = "Bubble Gum Simulator Infinity",
+        PlaceId = 85896571713843
+    }
+}
+
+local baseUrl = "https://raw.githubusercontent.com/Xan888/XanHub/main/"
+
+for fileName, gameInfo in pairs(games) do
+    if game.PlaceId == gameInfo.PlaceId then
+        print("Loading for:", gameInfo.Name)
+
+        local scriptUrl = baseUrl .. fileName .. ".lua"
+        loadstring(game:HttpGet(scriptUrl))()
+
+        break
+    end
 end
